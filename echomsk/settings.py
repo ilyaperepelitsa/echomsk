2389b2979f1e0d5852b15770d49b1d7bdcf64708
Ilya Perepelitsa <ilyaperepelitsa@gmail.com>
2019-10-03 17:51:20 +0300

    Updated interview.cpython-36.pyc.

--- echomsk/settings.py
+++ echomsk/settings.py
@@ -46,9 +46,9 @@ ROBOTSTXT_OBEY = False
 
 # Enable or disable spider middlewares
 # See https://doc.scrapy.org/en/latest/topics/spider-middleware.html
-#SPIDER_MIDDLEWARES = {
-#    'echomsk.middlewares.EchomskSpiderMiddleware': 543,
-#}
+SPIDER_MIDDLEWARES = {
+   'echomsk.middlewares.EchomskSpiderMiddleware': 543,
+}
 
 # Enable or disable downloader middlewares
 # See https://doc.scrapy.org/en/latest/topics/downloader-middleware.html
