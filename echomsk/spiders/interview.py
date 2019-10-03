ee65bcc094546e904c1f98823430701aaeeec484
Ilya Perepelitsa <ilyaperepelitsa@gmail.com>
2019-10-03 20:32:06 +0300

    Updated interview.py.

--- echomsk/spiders/interview.py
+++ echomsk/spiders/interview.py
@@ -132,7 +132,7 @@ class InterviewSpider(CrawlSpider):
                 interview_item['guest_title'] = entry['guest_title']
                 interview_item['host_name'] = entry['host_name']
                 interview_item['index'] = i[0]
-                interview_item['current_se'] = entry['date']
+                interview_item['speaker'] = entry['date']
                 interview_item['date'] = entry['date']
 
                 item = video_item.load_item()
