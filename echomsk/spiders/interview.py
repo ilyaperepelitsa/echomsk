68fbb021e12333e3faac1f828b733da875b4193f
Ilya Perepelitsa <ilyaperepelitsa@gmail.com>
2019-10-03 20:34:15 +0300

    Updated interview.py.

--- echomsk/spiders/interview.py
+++ echomsk/spiders/interview.py
@@ -134,7 +134,7 @@ class InterviewSpider(CrawlSpider):
                 interview_item['index'] = i[0]
                 interview_item['speaker'] = i[1]
                 interview_item['paragraph'] = i[2]
-                interview_item['url'] = i[2]
+                interview_item['url'] = respone.url()
 
                 item = interview_item.load_item()
                 yield item
