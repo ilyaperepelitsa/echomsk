ac4a703f7845b5a29fc17ce0fc5d74c97014f910
Ilya Perepelitsa <ilyaperepelitsa@gmail.com>
2019-10-03 20:33:41 +0300

    Updated models.py.

--- echomsk/models.py
+++ echomsk/models.py
@@ -49,7 +49,7 @@ class InterviewParagraph(Base_item):
         %(self.id, self.date, self.guest_name,
             self.guest_title,
             self.host_name, self.index,
-            self.speaker, self.paragraph)
+            self.speaker, self.paragraph, self.rl)
 
 
 # class FailedBMIDs(Base_item):
