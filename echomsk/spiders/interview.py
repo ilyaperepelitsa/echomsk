01bae1ab00269da9068266e70b5c993511750a4a
Ilya Perepelitsa <ilyaperepelitsa@gmail.com>
2019-10-03 19:16:29 +0300

    Updated interview.py.

--- echomsk/spiders/interview.py
+++ echomsk/spiders/interview.py
@@ -85,7 +85,7 @@ class InterviewSpider(CrawlSpider):
                 # current_text = ""
                 # current_speaker = ""
         # print(whole_interview)
-        for i , k in enumerate(in whole_interview):
+        for i , k in enumerate(whole_interview):
             print(str(i) + "__  " + k)
 
 
