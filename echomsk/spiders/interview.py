1cd36115632ec3f4c2d36c2a9a18b934055c896a
Ilya Perepelitsa <ilyaperepelitsa@gmail.com>
2019-10-03 16:03:38 +0300

    Updated interview.py.

--- echomsk/spiders/interview.py
+++ echomsk/spiders/interview.py
@@ -44,7 +44,7 @@ class InterviewSpider(scrapy.Spider):
             if index < len(text):
                 next_chunk = clean_chunk(text[index + 1])
                 if len(next_chunk) != 1:
-                    whole_interview.a
+                    whole_interview.append(index, )
 
 
 
