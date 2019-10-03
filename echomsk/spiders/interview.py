be6ea04d04780933c9e0fa7e8d08f826d3d405bb
Ilya Perepelitsa <ilyaperepelitsa@gmail.com>
2019-10-03 20:48:33 +0300

    Updated interview.py.

--- echomsk/spiders/interview.py
+++ echomsk/spiders/interview.py
@@ -147,7 +147,7 @@ class InterviewSpider(CrawlSpider):
                     interview_item.add_value('paragraph', str(i[2]))
                     # ['paragraph'] = str(i[2])
                     # .encode('ascii','ignore')
-                    interview_item.add_value('bmid', meta['bmid'])
+                    interview_item.add_value('url', meta['bmid'])
                     ['url'] = respone.url()
 
                     item = interview_item.load_item()
