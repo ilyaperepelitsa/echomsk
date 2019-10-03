fb504c7a81f948e6bc89a5276c03ad7e8949e430
Ilya Perepelitsa <ilyaperepelitsa@gmail.com>
2019-10-03 20:20:24 +0300

    Updated interview.py.

--- echomsk/spiders/interview.py
+++ echomsk/spiders/interview.py
@@ -12,7 +12,7 @@ import re
 
 from scrapy.loader import ItemLoader
 import json
-from echomsk.items import ApiItem
+from echomsk.items import InterviewParagraph
 from echomsk.models import *
 from echomsk.functions import *
 
