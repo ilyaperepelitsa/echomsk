5af853296186f1d30c1d908d483e6a611385192d
Ilya Perepelitsa <ilyaperepelitsa@gmail.com>
2019-10-03 18:29:18 +0300

    Updated interview.py.

--- echomsk/spiders/interview.py
+++ echomsk/spiders/interview.py
@@ -62,10 +62,7 @@ class InterviewSpider(CrawlSpider):
         current_text = ""
         current_speaker = ""
         for index, paragraph in enumerate(text):
-            par = Selector(text=paragraph).xpath('//*').getall()
-            for i in par:
-                # chunk = Selector(text=i)
-                print(i)
+            p
                 # print(i.xpath('name'))
             # chunk_name = chnk.xpath('name()')
             # print(paragraph)
