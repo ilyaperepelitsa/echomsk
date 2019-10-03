58e8ae580f14572ca7addc4e43a1fd419e23bd21
Ilya Perepelitsa <ilyaperepelitsa@gmail.com>
2019-10-03 15:49:21 +0300

    Updated interview.py.

--- echomsk/spiders/interview.py
+++ echomsk/spiders/interview.py
@@ -28,7 +28,7 @@ class InterviewSpider(scrapy.Spider):
             chunk = Selector(text=paragraph).xpath('//text()').getall()
             if len(chunk) > 1:
                 current_speaker = chunk[0]
-                current_text += chunk
+                current_text = chunk[-1]
 
 
         # text = [i.replace('\r\n', '').strip() for i in text]
