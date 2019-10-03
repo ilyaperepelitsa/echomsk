9c92f9abdd6fe3e9f5137d17d798211d047171ca
Ilya Perepelitsa <ilyaperepelitsa@gmail.com>
2019-10-03 20:17:25 +0300

    Updated models.py.

--- echomsk/models.py
+++ echomsk/models.py
@@ -43,7 +43,7 @@ class InterviewParagraph(Base_item):
     def __repr__(self):
         return "<Base_item(id='%s', date='%s', guest_name='%s',\
                         guest_title='%s', host_name='%s', index='%s',\
-                        title='%s', subtitle='%s',\
+                        speaker='%s', subtitle='%s',\
                         title_pos='%s',\
                         video_list_size='%s', basic_index='%s',\
                         terminal_title='%s', terminal_title_pos='%s')>"\
