cdf742ccc94c588268d9dcf6b3d741c97f00d291
Ilya Perepelitsa <ilyaperepelitsa@gmail.com>
2019-10-03 19:32:17 +0300

    Updated interview.py.

--- echomsk/spiders/interview.py
+++ echomsk/spiders/interview.py
@@ -74,7 +74,7 @@ class InterviewSpider(CrawlSpider):
 
             if (index + 1) < len(text):
                 next_chunk = clean_chunk(text[index + 1])
-                if len(next_chunk) != 1:
+                if len(next_chunk) != 1 and len(*):
                     whole_interview.append([index, current_speaker,
                                                         current_text])
                     current_text = ""
