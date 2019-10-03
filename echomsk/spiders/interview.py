de41e873bf0338f85bcaac5f1fee305d67b0016f
Ilya Perepelitsa <ilyaperepelitsa@gmail.com>
2019-10-03 20:46:57 +0300

    Updated interview.py.

--- echomsk/spiders/interview.py
+++ echomsk/spiders/interview.py
@@ -131,8 +131,9 @@ class InterviewSpider(CrawlSpider):
 
                 for i in entry['interview']:
                     interview_item = ItemLoader(item = InterviewParagraph(), response = response)
-                    .add_value('bmid', meta['bmid'])
-                    interview_item['date'] = entry['date']
+
+                    interview_item.add_value('bmid', meta['bmid'])
+                    ['date'] = entry['date']
                     interview_item['guest_name'] = entry['guest_name']
                     interview_item['guest_title'] = entry['guest_title']
                     interview_item['host_name'] = entry['host_name']
