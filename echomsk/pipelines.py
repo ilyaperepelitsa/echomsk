35a969231c7e1a271e0a2aafd44d7df1d68b4929
Ilya Perepelitsa <ilyaperepelitsa@gmail.com>
2019-10-04 00:41:15 +0300

    Updated pipelines.py.

--- echomsk/pipelines.py
+++ echomsk/pipelines.py
@@ -13,14 +13,14 @@ class EchomskPipeline(object):
     def process_item(self, item, spider):
 
 
-                        date = item["date"][0],
-                        guest_name = item["guest_name"][0],
-                        guest_title = item["guest_title"][0],
-                        host_name = item["host_name"][0],
-                        index = item["index"][0],
-                        speaker = item["speaker"][0],
-                        text : item["text"][0],
-                        url : item["url"][0]
+        date = item["date"][0],
+        guest_name = item["guest_name"][0],
+        guest_title = item["guest_title"][0],
+        host_name = item["host_name"][0],
+        index = item["index"][0],
+        speaker = item["speaker"][0],
+        text = item["text"][0],
+        url = item["url"][0]
 
 
         data_entry = {"date" = item["date"][0],
