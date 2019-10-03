db0dd90aedba7d017121e591b7e7c058e5245af3
Ilya Perepelitsa <ilyaperepelitsa@gmail.com>
2019-10-03 17:44:23 +0300

    Updated interview.py.

--- echomsk/spiders/interview.py
+++ echomsk/spiders/interview.py
@@ -19,7 +19,7 @@ class InterviewSpider(scrapy.Spider):
     allowed_domains = ['echo.msk.ru']
     start_urls = ['https://echo.msk.ru/programs/personalno/']
 
-    rules =(
+    rules =
         Rule(
             LinkExtractor(
                 restrict_xpaths = '//*[@class="pager"]',
