e768e4653e24bd93f9ed331a1131fd2585607042
Ilya Perepelitsa <ilyaperepelitsa@gmail.com>
2019-10-03 15:38:23 +0300

    Updated interview.py.

--- echomsk/spiders/interview.py
+++ echomsk/spiders/interview.py
@@ -21,6 +21,7 @@ class InterviewSpider(scrapy.Spider):
         //div[contains(@class, "lead")]//a//text()
 
         text = response.xpath('//div[@class="mmcontainer"]//p').getall()
+        for i i
         text = [i.replace('\r\n', '').strip() for i in text]
         # text = [i for i in text if i != "\r\n"]
         text = [re.sub(r'^НОВОСТИ|новости$', '', i) for i in text]
