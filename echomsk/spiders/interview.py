fb0622775fdb153146e1ec1826201e858546b026
Ilya Perepelitsa <ilyaperepelitsa@gmail.com>
2019-10-03 17:14:23 +0300

    Updated interview.py.

--- echomsk/spiders/interview.py
+++ echomsk/spiders/interview.py
@@ -86,7 +86,7 @@ class InterviewSpider(scrapy.Spider):
         # text = [re.sub(r'^НОВОСТИ|новости$', '', i) for i in text]
         # text = [i.strip() for i in text]
         # text = [i for i in text if len(i) > 1]
-        pass
+        # pass
 
 
 
