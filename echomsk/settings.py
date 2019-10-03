66beb918234c0dbbd5617734b9e583b47ed01cf1
Ilya Perepelitsa <ilyaperepelitsa@gmail.com>
2019-10-03 16:56:06 +0300

    Updated settings.py.

--- echomsk/settings.py
+++ echomsk/settings.py
@@ -19,7 +19,7 @@ NEWSPIDER_MODULE = 'echomsk.spiders'
 #USER_AGENT = 'echomsk (+http://www.yourdomain.com)'
 
 # Obey robots.txt rules
-ROBOTSTXT_OBEY = True
+ROBOTSTXT_OBEY = False
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
 #CONCURRENT_REQUESTS = 32
