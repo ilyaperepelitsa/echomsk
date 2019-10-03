22b64c6f20d5e7e219eddf4dc896808b6d8a4d3c
Ilya Perepelitsa <ilyaperepelitsa@gmail.com>
2019-10-04 00:40:31 +0300

    Updated pipelines.py.

--- echomsk/pipelines.py
+++ echomsk/pipelines.py
@@ -15,7 +15,7 @@ class EchomskPipeline(object):
 
                         date : item["date"][0],
                         guest_name : item["guest_name"][0],
-                        "guest_title" : item["guest_title"][0],
+                        guest_title : item["guest_title"][0],
                         "host_name" : item["host_name"][0],
                         "index" : item["index"][0],
                         "speaker" : item["speaker"][0],
