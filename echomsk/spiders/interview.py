603ade88432e9f4ef60d6e83a48e6606fb958350
Ilya Perepelitsa <ilyaperepelitsa@gmail.com>
2019-10-03 18:14:10 +0300

    Updated interview.py.

--- echomsk/spiders/interview.py
+++ echomsk/spiders/interview.py
@@ -56,7 +56,7 @@ class InterviewSpider(CrawlSpider):
         # # host name
         # //div[contains(@class, "lead")]//a//text()
 
-        text = response.xpath('//div[@class="mmplayer"]//div[contains(@class, "lead")]//p').getall()
+        text = response.xpath('//div[@class="mmplayer"]//div[contains(@class, "typical")]//*').getall()
         whole_interview = []
         current_text = ""
         current_speaker = ""
