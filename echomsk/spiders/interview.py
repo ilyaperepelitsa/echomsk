576716ddacd158d4396c7be944fda0ba3ddeb07d
Ilya Perepelitsa <ilyaperepelitsa@gmail.com>
2019-10-03 17:41:45 +0300

    Updated interview.py.

--- echomsk/spiders/interview.py
+++ echomsk/spiders/interview.py
@@ -30,7 +30,7 @@ class InterviewSpider(scrapy.Spider):
             LinkExtractor(
                 restrict_xpaths = '//*[@class="content"]//div[@class="rel"]//div[contains(@class, "preview")]//*[@class="txt"]',
                 canonicalize = True,
-                unique=True
+                unique=True 
             ), callback="parse_interview")
             )
 
