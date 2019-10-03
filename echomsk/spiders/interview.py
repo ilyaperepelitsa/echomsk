965856abcc66d249a0ca9cc5f8b44f0e092a540d
Ilya Perepelitsa <ilyaperepelitsa@gmail.com>
2019-10-03 15:59:40 +0300

    Updated interview.py.

--- echomsk/spiders/interview.py
+++ echomsk/spiders/interview.py
@@ -39,9 +39,9 @@ class InterviewSpider(scrapy.Spider):
 
 
             next_chunk = text[index + 1]
-            next_chunk = Selector(text=paragraph).xpath('//text()').getall()
-            chunk = [i.replace('\r\n', '').strip() for i in chunk]
-            chunk = [re.sub(r'^НОВОСТИ|новости$', '', i) for i in chunk]
+            next_chunk = Selector(text=next_chunk).xpath('//text()').getall()
+            chunk = [i.replace('\r\n', '').strip() for i in next_chunk]
+            chunk = [re.sub(r'^НОВОСТИ|новости$', '', i) for i in next_chunk]
             chunk = [i.strip() for i in chunk]
             chunk = [i for i in chunk if len(i) > 1]
 
